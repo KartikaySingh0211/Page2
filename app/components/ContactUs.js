424ac274248c01380a784dc1bf41feb586e221e4
Kartikay Singh <kartikay.singh.0211@gmail.com>
2024-11-13 18:48:42 +0530

    Few changes in page 2

--- app/components/ContactUs.js
+++ app/components/ContactUs.js
@@ -41,16 +41,40 @@ function ContactUs() {
 							className="w-1/2 p-2  border-gray-600 border bg-[#d9d9d9] rounded-3xl pl-5 focus:outline-none focus:border-gray-600"
 						/>
 					</div>
-					<input
-						type="email"
-						placeholder="Your email"
-						className="w-full p-2  border-gray-600 border bg-[#d9d9d9] rounded-3xl pl-5 focus:outline-none focus:border-gray-600"
-					/>
-					<input
-						type="text"
-						placeholder="Phone number"
-						className="w-full p-2 border-gray-600 border bg-[#d9d9d9] rounded-3xl pl-5 focus:outline-none focus:border-gray-600"
-					/>
+					<div class="relative">
+						<input
+							type="email"
+							placeholder="Your email"
+							class="w-full pl-10 py-2 border border-gray-600 bg-[#d9d9d9] rounded-3xl focus:outline-none focus:border-gray-600"
+						/>
+						<span class="absolute top-1/2 left-4 transform -translate-y-1/2 text-gray-400">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+							>
+								<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+								<path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+							</svg>
+						</span>
+					</div>
+					<div class="relative">
+						<div class="absolute left-4 top-1/2 transform -translate-y-1/2 text-black">
+							<select className="bg-inherit focus:outline-none">
+								<option value="India">+91</option>
+								<option value="USA">+1</option>
+								<option value="UK">+44</option>
+								<option value="Australia">+61</option>
+								<option value="Canada">+1</option>
+							</select>
+						</div>
+						<input
+							type="tel"
+							placeholder="Phone number"
+							class="w-full pl-20 pr-4 py-2 border-gray-600 border bg-[#d9d9d9] rounded-3xl focus:outline-none focus:border-gray-600"
+						/>
+					</div>
 					<div className="relative">
 						<textarea
 							value={message}
--- app/constants/index.js
+++ app/constants/index.js
@@ -18,9 +18,11 @@ export const navLinks = [
 ];
 
 export const cards = [
-	{ id: 1, firstname: "John", lastname: "Doe", role: "tbd" },
-	{ id: 2, firstname: "John", lastname: "Doe", role: "tbd" },
-	{ id: 3, firstname: "John", lastname: "Doe", role: "tbd" },
-	{ id: 4, firstname: "John", lastname: "Doe", role: "tbd" },
-	{ id: 5, firstname: "John", lastname: "Doe", role: "tbd" },
+	{ id: 1, firstname: "John", lastname: "Doe", role: "Founder" },
+	{ id: 2, firstname: "Mary", lastname: "Jane", role: "Co-founder" },
+	{ id: 3, firstname: "Alex", lastname: "Parker", role: "Designer" },
+	{ id: 4, firstname: "Jane", lastname: "Doe", role: "Developer" },
+	{ id: 5, firstname: "John", lastname: "Smith", role: "Tester" },
+	{ id: 6, firstname: "Jane", lastname: "Smith", role: "Manager" },
+	{ id: 7, firstname: "Alex", lastname: "Doe", role: "CEO" },
 ];
